fdfc4d2ba666dd1d740913cfc84948b1bc93308a
unknown <iamfarmanmalik@gmail.com>
2023-01-30 22:14:38 +0530

    modified Order model

--- model/order.model.js
+++ model/order.model.js
@@ -3,7 +3,8 @@ const mongoose=require("mongoose");
 const orderSchema=mongoose.Schema({
     product:{type:mongoose.Types.ObjectId,ref:"product",required:true},
     user:{type:mongoose.Types.ObjectId,ref:"user",required:true},
-    status:{type:String,default:"Pending"}
+    status:{type:String,default:"Pending"},
+    date:{type:Date,default:Date.now()}
 })
 
 const Ordermodel=mongoose.model("order",orderSchema);
