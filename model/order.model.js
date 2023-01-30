ef498dba118f5c4a0d8f88899a9bb895bcec486f
unknown <iamfarmanmalik@gmail.com>
2023-01-30 21:54:23 +0530

    Made changes in order status

--- model/order.model.js
+++ model/order.model.js
@@ -2,7 +2,8 @@ const mongoose=require("mongoose");
 
 const orderSchema=mongoose.Schema({
     product:{type:mongoose.Types.ObjectId,ref:"product",required:true},
-    user:{type:mongoose.Types.ObjectId,ref:"user",required:true}
+    user:{type:mongoose.Types.ObjectId,ref:"user",required:true},
+    status:{type:String,default:"Pending"}
 })
 
 const Ordermodel=mongoose.model("order",orderSchema);
